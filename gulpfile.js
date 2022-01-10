7f9f88a8e5a578c0cf374136130a8d2fa677c224
Menat Pink <pink@Pinks-MacBook-Pro.local>
2022-01-10 15:44:52 +0000

    Addition of small changes to gulpfile and Tajam

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var sass = require("gulp-sass")(require('sass'));
 var concat = require("gulp-concat-css");
 
 gulp.task("sass", function() {
@@ -9,10 +9,21 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("assets/css"));
 });
 
+gulp.task("tajam-sass", function() {
+  return gulp
+    .src("Tajam/src/CSS/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("Tajam/CSS"));
+});
+
 gulp.task("gulp-check", function() {
   return console.log("Gulp Working, on changing Sass files");
 });
 
 gulp.task("watch", function() {
   gulp.watch("assets/sass/custom/*.scss", gulp.series("sass"));
+});
+
+gulp.task("tajam-watch", function() {
+  gulp.watch("Tajam/src/CSS/*.scss", gulp.series("tajam-sass"));
 });
