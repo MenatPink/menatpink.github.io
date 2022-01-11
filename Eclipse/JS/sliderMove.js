4e0377f2ce862b1add7bd7c6165b3b4ec645eecc
Menat Pink <pink@Pinks-MacBook-Pro.local>
2022-01-11 19:48:56 +0000

    Major responsiveness changes to parts of site

---
+++ Eclipse/JS/sliderMove.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var wScroll = $(this).scrollTop();
+
+		if (wScroll > $('.section3').offset().top - $(window).height() / 4) {
+			$('.featureWrapper').each(function (i) {
+				setTimeout(function () {
+					$('.featureWrapper').eq(i).addClass('isShowing');
+				}, 150 * (i + 1));
+			});
+		}
+	});
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,45 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require('sass'));
-var concat = require("gulp-concat-css");
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
 
-gulp.task("sass", function() {
-  return gulp
-    .src("assets/sass/custom/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("assets/css"));
+// TASKS FOR OVERALL SCRIPT
+gulp.task('general-sass', function () {
+	return gulp
+		.src('assets/sass/custom/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('assets/css'));
 });
 
-gulp.task("tajam-sass", function() {
-  return gulp
-    .src("Tajam/src/CSS/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("Tajam/CSS"));
+gulp.task('general-gulp-check', function () {
+	return console.log('Gulp Working, on changing Sass files');
 });
 
-gulp.task("gulp-check", function() {
-  return console.log("Gulp Working, on changing Sass files");
+gulp.task('general-watch', function () {
+	gulp.watch('assets/sass/custom/*.scss', gulp.series('general, sass'));
 });
 
-gulp.task("watch", function() {
-  gulp.watch("assets/sass/custom/*.scss", gulp.series("sass"));
+// Tasks for Tajam
+gulp.task('tajam-sass', function () {
+	return gulp
+		.src('Tajam/src/CSS/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('Tajam/CSS'));
 });
 
-gulp.task("tajam-watch", function() {
-  gulp.watch("Tajam/src/CSS/*.scss", gulp.series("tajam-sass"));
+gulp.task('tajam-watch', function () {
+	gulp.watch('Tajam/src/CSS/*.scss', gulp.series('tajam-sass'));
+});
+
+//Tasks for Eclipse
+gulp.task('eclipse-sass', function () {
+	return gulp
+		.src('Eclipse/CSS/src/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('Eclipse/CSS/dest'));
+});
+
+gulp.task('eclipse-watch', function () {
+	gulp.watch(
+		'Eclipse/CSS/src/*.scss',
+		gulp.series('eclipse-sass'),
+	);
 });
